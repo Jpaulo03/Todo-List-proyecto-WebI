a9f73db807446889d2db4db075f8cf7cc280015d
Jpaulo03 <paulitomb03@gmail.com>
2026-02-25 00:08:51 -0400

    Terminado el html base, se agrego el js y la funcion de agregar tarea

---
+++ script.js
@@ -0,0 +1,40 @@
+const inputBuscador = document.querySelector(".agregar-item");
+const botonAgregar = document.querySelector(".btn-agregar");
+
+const cajaTareas = document.querySelector(".caja-contenido");
+const mensajeVacio = document.querySelector(".mensaje-vacio");
+const consejos = document.querySelector(".consejos");
+
+
+function mostrarTarea(textoDeLaTarea) {
+    if(mensajeVacio) mensajeVacio.style.display = "none";
+    if(consejos) consejos.style.display = "none";
+
+    const divTarea = document.createElement("div");
+    divTarea.classList.add("tarea-item");
+
+    divTarea.innerHTML= `
+    <button class="btn-completar"></button>
+    <span class="texto-tarea">${textoDeLaTarea}</span>
+    <button class="btn-eliminar">X</>
+    `;
+
+    cajaTareas.appendChild(divTarea);
+}
+
+
+function agregarTarea() {
+    const nombreTarea = inputBuscador.value.trim();
+
+    if(nombreTarea !== "") {
+        console.log("Se capturo la tarea: ", nombreTarea);
+
+        mostrarTarea(nombreTarea);
+
+        inputBuscador.value = "";
+    } else {
+        console.log("Campo de texto vacio")
+    }
+}
+
+botonAgregar.addEventListener("click", agregarTarea);
